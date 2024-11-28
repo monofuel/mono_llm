4903bed913e5a9f62798b27de8038aefebff2d5d
monofuel <monofuel@japura.net>
2024-11-28 15:45:03 -0500

    add proxy example

---
+++ examples/proxy.nim
@@ -0,0 +1,20 @@
+import mono_llm
+
+# Simplelogging  proxy example for mono_llm
+
+let
+  endpoint = "https://api.openai.com/v1"
+  address = "localhost"
+  port = 8090.uint16
+  logFile = "gateway.log"
+
+
+let gateway = newOpenAIGateway(
+  endpoint,
+  address,
+  port,
+  logFile
+  )
+
+
+gateway.start()
